58da692039213840f0db9c3fb3b0cf2652e73713
Ярослав Чернышов <katacan228@gmail.com>
2026-03-01 21:39:03 +0300

    init commit

---
+++ src/main/java/ru/katacan/registrationoffice/RegistrationOfficeApplication.java
@@ -0,0 +1,13 @@
+package ru.katacan.registrationoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationOfficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationOfficeApplication.class, args);
+	}
+
+}
